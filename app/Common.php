c0e239cb742737e03c3fe077f90ce18c3692ea64
Subhendu <81496993+Subhendu1987@users.noreply.github.com>
2024-10-29 23:46:55 +0530

    Update Common.php

--- app/Common.php
+++ app/Common.php
@@ -70,7 +70,7 @@ function aboutdetails()
         'sitedetails' => [
             'Name' => "Streamdex",
             'Org' => "Dex Corp",
-            'Link' => "https://github.com/Subhendu1987/StreamDex/blob/main/README.md"
+            'Link' => "https://subhendu1987.github.io/DexCorp/"
         ],
         'version' => "1.0.1 (beta)"
 
