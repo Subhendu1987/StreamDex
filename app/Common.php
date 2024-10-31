c5dbaca1523e0557ace9650e3af110bed91827bc
Subhendu <81496993+Subhendu1987@users.noreply.github.com>
2024-10-31 21:15:48 +0530

    Update Common.php

--- app/Common.php
+++ app/Common.php
@@ -56,23 +56,23 @@ function aboutdetails()
 {
     $adt = [
         'gitdetails' => [
-            'Name' => "Streamdex",
-            'Link' => "https://github.com/Subhendu1987/StreamDex"
+            'Name' => "GitHub - Streamdex",
+            'Link' => "https://github.com/your-repo-link"
         ],
         'emaildetails' => [
             'Name' => "Streamdex",
             'Link' => "support@streamdex.com"
         ],
         'ytdetails' => [
-            'Name' => "Subho Codes",
-            'Link' => "https://www.youtube.com/@subhocodes"
+            'Name' => "Your Channel",
+            'Link' => "https://www.youtube.com/your-channel-link"
         ],
         'sitedetails' => [
             'Name' => "Streamdex",
             'Org' => "Dex Corp",
-            'Link' => "https://subhendu1987.github.io/DexCorp/"
+            'Link' => "https://www.dexcorp.com"
         ],
-        'version' => "1.0.1 (beta)"
+        'version' => "0.1.2 (beta)"
 
     ];
     
@@ -108,7 +108,7 @@ function streamstartcmd($plid)
 
         if(!empty($mediainfo) && !empty($linkinfo)){
             // Initialize the FFmpeg command
-            $ffmpegCommand = "ffmpeg ";
+            $ffmpegCommand = "ffmpeg -re ";
 
             // Handle looping logic
             if ($Playlistinfo->is_looped == 1) {
@@ -125,7 +125,7 @@ function streamstartcmd($plid)
             $ffmpegCommand .= "-b:v ".$settingsinfo->video_bitrate."k ";                // Set target video bitrate
             $ffmpegCommand .= "-maxrate ".$settingsinfo->maximum_bitrate."k ";          // Set maximum bitrate
             $ffmpegCommand .= "-bufsize ".$settingsinfo->buffer_size."k ";              // Set buffer size
-            $ffmpegCommand .= "-vf 'scale=".$Resulationinfo->str_resulution_height.":".$Resulationinfo->str_resulution_width."' ";  // Set video resolution
+            $ffmpegCommand .= "-vf 'scale=".$Resulationinfo->str_resulution_width.":".$Resulationinfo->str_resulution_height."' ";  // Set video resolution
             $ffmpegCommand .= "-c:a aac ";                                              // Use AAC audio codec
             $ffmpegCommand .= "-b:a ".$settingsinfo->audio_bitrate."k ";                // Set audio bitrate to 128 kbps
             $ffmpegCommand .= "-ar ".$settingsinfo->audio_sample_rate." ";              // Set audio sample rate to 44.1 kHz
@@ -155,25 +155,7 @@ function streamstartcmd($plid)
     }
 }
 
-function streamstopcmd($plid) 
-{
-    // Check if user session exists
-    if (!session()->get('user')) {
-        return ""; // Return a message if no user session
-    }
-    
-    // Get the database instance
-    $db = \Config\Database::connect(); 
-    // Get the unique stream identifier
-    $Playlistidentity = $db->query("SELECT uniq_identity FROM Playlist WHERE id= ".$plid)->getRow();
-    if($Playlistidentity){
-        // Generate the stop command to kill the process associated with the unique tag
-        $stopCommand = "ps aux | grep ffmpeg | grep " . escapeshellarg($Playlistidentity->uniq_identity) . " | grep -v grep | awk '{print $2}' | xargs kill -9";
-        $db->query("UPDATE Playlist SET status = 0 WHERE id = ".$plid); 
-        exec(trim($stopCommand)); // Return the stop command
-    }           
-    
-}
+
 
 function startMVCommand($musicVideoId) {
     $db = \Config\Database::connect();
@@ -210,7 +192,7 @@ function startMVCommand($musicVideoId) {
     }
 
     // Prepare the FFmpeg command
-    $command = "ffmpeg ";
+    $command = "ffmpeg -re ";
 
     // Video input
     $videoPath = $musicVideo->video_path;
@@ -231,7 +213,7 @@ function startMVCommand($musicVideoId) {
         if (count($audioInputs) == 1) {
             // One audio, infinite loop
             $command .= "-i " . escapeshellarg($audioInputs[0]) . " ";
-            $command .= '-filter_complex "[1:a]aloop=loop=-1:size=2e+9[audio]; [0:v]scale=' . $Resulationinfo->str_resulution_width . ':' . $Resulationinfo->str_resulution_height . '[video]" ';
+            $command .= '-filter_complex "[1:a]aloop=loop=-1:size=2e+9[audio]; [0:v]scale=' . $Resulationinfo->str_resulution_height . ':' . $Resulationinfo->str_resulution_width . '[video]" ';
             $command .= '-map "[video]" -map "[audio]" ';
         } else {
             // Multiple audio files, infinite loop
@@ -250,7 +232,7 @@ function startMVCommand($musicVideoId) {
             $filterComplex .= $concatInputs . "concat=n=" . count($audioInputs) . ":v=0:a=1,aloop=loop=-1:size=2e+9[audio]; ";
             
             // Scale the video input
-            $filterComplex .= "[0:v]scale=" . $Resulationinfo->str_resulution_width . ":" . $Resulationinfo->str_resulution_height . "[video]";
+            $filterComplex .= "[0:v]scale=" . $Resulationinfo->str_resulution_height . ":" . $Resulationinfo->str_resulution_width . "[video]";
             
             $command .= '-filter_complex "' . $filterComplex . '" ';
             
@@ -261,14 +243,18 @@ function startMVCommand($musicVideoId) {
         if (count($audioInputs) == 1) {
             // One audio, play once
             $command .= "-i " . escapeshellarg($audioInputs[0]) . " ";
-            $command .= '-vf "scale=' . $Resulationinfo->str_resulution_width . ':' . $Resulationinfo->str_resulution_height . '" ';
+            $command .= '-map 0:v -map 1:a -vf "scale=' . $Resulationinfo->str_resulution_height . ':' . $Resulationinfo->str_resulution_width . '" ';
             $command .= '-shortest ';
         } else {
             // Multiple audio files, play once
-            $audioConcat = implode("|", array_map('escapeshellarg', $audioInputs));
-            $command .= '-i "concat:' . $audioConcat . '" ';
-            $command .= '-vf "scale=' . $Resulationinfo->str_resulution_width . ':' . $Resulationinfo->str_resulution_height . '" ';
-            $command .= '-shortest ';
+            $command .= implode(' ', array_map(fn($audioPath) => "-i " . escapeshellarg($audioPath), $audioInputs)) . " ";
+
+            $filterComplex = [];
+            foreach (range(1, count($audioInputs)) as $i) {
+                $filterComplex[] = "[$i:a]";
+            }
+            $command .= '-filter_complex "' . implode('', $filterComplex) . 'concat=n=' . count($audioInputs) . ':v=0:a=1[audio]; ';
+            $command .= '[0:v] scale=' . $Resulationinfo->str_resulution_height . ':' . $Resulationinfo->str_resulution_width . '[video]; [video][audio]concat=n=1:v=1:a=1[v][a]" -map "[v]" -map "[a]" -shortest ';            
         }
     }
 
@@ -294,15 +280,35 @@ function startMVCommand($musicVideoId) {
     $command = "nohup sh -c ' " . $command . " && curl -X POST \"" . base_url('webhook/Musicvideo/') . $uniqueTag . "\"' > /dev/null 2>&1 & disown";
     
     createstreamlink($uniqueTag, $rtmp);
+
+    
     
     exec(trim($command));
     
     // Update stream status
     $db->query("UPDATE music_video SET mv_stream_status = 1 WHERE id = " . $musicVideoId);
-
+    return (trim($command));
 }
 
-
+function streamstopcmd($plid) 
+{
+    // Check if user session exists
+    if (!session()->get('user')) {
+        return ""; // Return a message if no user session
+    }
+    
+    // Get the database instance
+    $db = \Config\Database::connect(); 
+    // Get the unique stream identifier
+    $Playlistidentity = $db->query("SELECT uniq_identity FROM Playlist WHERE id= ".$plid)->getRow();
+    if($Playlistidentity){
+        // Generate the stop command to kill the process associated with the unique tag
+        $stopCommand = "ps aux | grep ffmpeg | grep " . escapeshellarg($Playlistidentity->uniq_identity) . " | grep -v grep | awk '{print $2}' | xargs kill -9";
+        $db->query("UPDATE Playlist SET status = 0 WHERE id = ".$plid); 
+        exec(trim($stopCommand)); // Return the stop command
+    }           
+    
+}
 
 function stopMVCommand($musicVideoId) {
     // Check if user session exists
