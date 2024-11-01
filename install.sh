4fd5d3b90d0f077cf25c5b9aea3c2fcd235b24fa
Subhendu <81496993+Subhendu1987@users.noreply.github.com>
2024-11-01 14:00:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,26 +43,13 @@ sudo apt update && sudo apt upgrade -y
 echo "Installing Nginx..."
 sudo apt install nginx -y
 
+
 # Install required PHP and FFmpeg packages
 echo "Installing PHP, FFmpeg, and other required packages..."
-# Update the package list
-sudo apt update
-
-# Install software-properties-common to provide add-apt-repository
-sudo apt install software-properties-common -y
-
-# Run update again to ensure all repos are loaded
-sudo apt update
-
-# Add the Ondrej PHP PPA (this will prompt to confirm if you trust the source)
-sudo add-apt-repository ppa:ondrej/php -y
-
-# Update again to ensure the PPA is available
-sudo apt update
-
-# Install PHP and required packages
-sudo apt install curl ffmpeg git php8.1-fpm php8.1-sqlite3 php8.1-gd php8.1-intl php8.1-mbstring -y
-
+sudo apt install software-properties-common -y 
+sudo add-apt-repository ppa:ondrej/php -y 
+sudo apt update  
+sudo apt install curl ffmpeg git php8.1-fpm php8.1-sqlite3 php8.1-gd php8.1-intl php8.1-mbstring -y 
 
 
 # Start and enable PHP
